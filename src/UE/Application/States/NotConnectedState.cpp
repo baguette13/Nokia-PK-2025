6428b336e959149b80b72030c56894375cb488d7
baguette13 <kacper1c26@gmail.com>
2025-03-10 17:18:24 +0100

    After Lab2

--- src/UE/Application/States/NotConnectedState.cpp
+++ src/UE/Application/States/NotConnectedState.cpp
@@ -1,4 +1,5 @@
 #include "NotConnectedState.hpp"
+#include "ConnectingState.hpp"
 
 namespace ue
 {
@@ -8,5 +9,15 @@ NotConnectedState::NotConnectedState(Context &context)
 {
 
 }
+
+void NotConnectedState::handleSib(common::BtsId btsId)
+{
+    using namespace std::chrono_literals;
+    context.bts.sendAttachRequest(btsId);
+    context.timer.startTimer(500ms);
+
+    context.setState<ConnectingState>();
+}
+
 
 }
